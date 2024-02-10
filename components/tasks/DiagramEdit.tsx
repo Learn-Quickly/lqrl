b5be2cf44d64b10d610770a260bb59e080ee8136
Serhii <serhii.kaluhin@nure.ua>
2024-02-10 02:20:38 +0200

    add drag-n-drop

--- components/tasks/DiagramEdit.tsx
+++ components/tasks/DiagramEdit.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import ReactFlow, {
+  ReactFlowProvider,
   Background,
   BackgroundVariant,
   Controls,
@@ -19,7 +20,35 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
+function Sidebar() {
+  const onDragStart = (event: any, nodeType: any) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <aside className="flex gap-4 p-4 md:h-full md:flex-col">
+      <div className="description">
+        Add nodes to the diagram by dragging them from the sidebar
+      </div>
+      <div onDragStart={(event) => onDragStart(event, "input")} draggable>
+        Input Node
+      </div>
+      <div onDragStart={(event) => onDragStart(event, "default")} draggable>
+        Default Node
+      </div>
+      <div onDragStart={(event) => onDragStart(event, "output")} draggable>
+        Output Node
+      </div>
+    </aside>
+  );
+}
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
 export function DiagramEdit() {
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
@@ -27,27 +56,63 @@ export function DiagramEdit() {
     (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges],
   );
+
+  const onDragOver = useCallback((event: any) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event: any) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      // check if the dropped element is valid
+      if (typeof type === "undefined" || !type) {
+        return;
+      }
+
+      // @ts-ignore
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance],
+  );
+
   return (
     <div className="flex h-full w-full flex-col md:flex-row">
-      <div className="flex gap-4 p-4 md:h-full md:flex-col">
-        <h1>Diagram controls</h1>
-        <h1>Diagram controls</h1>
-        <h1>Diagram controls</h1>
-      </div>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        proOptions={{ hideAttribution: true }}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        className="rounded-tl-2xl border border-stone-300 bg-stone-50"
-        fitView
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-      </ReactFlow>
+      <ReactFlowProvider>
+        <Sidebar />
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          proOptions={{ hideAttribution: true }}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          // @ts-ignore
+          onInit={setReactFlowInstance}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          className="rounded-tl-2xl border border-stone-300 bg-stone-50"
+          fitView
+        >
+          <Controls />
+          <MiniMap />
+          <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        </ReactFlow>
+      </ReactFlowProvider>
     </div>
   );
 }
